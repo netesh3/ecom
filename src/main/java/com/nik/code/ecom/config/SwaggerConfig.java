af7fd698aa507276f11181190051be26df81ce95
Kumar <netkumar@informatica.com>
2022-01-17 21:15:33 +0530

    refactor

--- src/main/java/com/nik/code/ecom/config/SwaggerConfig.java
+++ src/main/java/com/nik/code/ecom/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
     }
 
     private ApiInfo getApiInfo() {
-        Contact contact = new Contact("webtutsplus", "http://webtutsplus.com", "contact.webtutsplus@gmail.com");
+        Contact contact = new Contact("Ritik Tyagi", "http://google.com", "tyagiritik200@gmail.com");
         return new ApiInfoBuilder()
                 .title("Ecommerce API")
                 .description("Documentation Ecommerce api")
