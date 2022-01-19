d89169099875cd8ddf9ef7eca5f708b6811fa72a
Kumar <netkumar@informatica.com>
2022-01-19 14:03:11 +0530

    search service

---
+++ src/main/java/com/nik/code/ecom/controllers/SearchController.java
@@ -0,0 +1,25 @@
+package com.nik.code.ecom.controllers;
+
+import com.nik.code.ecom.dto.product.ProductDTO;
+import com.nik.code.ecom.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("search")
+@RestController
+public class SearchController {
+
+    @Autowired
+    SearchService searchService;
+
+    @GetMapping
+    public List<ProductDTO> findProductsByKeywords(@RequestParam String keyword) {
+        List<ProductDTO> productDTOS = searchService.searchProducts(keyword);
+        return productDTOS;
+    }
+}
---
+++ src/main/java/com/nik/code/ecom/service/SearchService.java
@@ -0,0 +1,15 @@
+package com.nik.code.ecom.service;
+
+import com.nik.code.ecom.dto.product.ProductDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchService {
+
+    public List<ProductDTO> searchProducts(String keyword) {
+
+        return null;
+    }
+}
