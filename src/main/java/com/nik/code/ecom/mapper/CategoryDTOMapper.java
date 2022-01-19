2191858077121bc8aac36c8c91661434e502cfa4
Kumar <netkumar@informatica.com>
2022-01-19 13:28:00 +0530

    nik commit missing pkg

---
+++ src/main/java/com/nik/code/ecom/mapper/CategoryDTOMapper.java
@@ -0,0 +1,37 @@
+package com.nik.code.ecom.mapper;
+
+import com.nik.code.ecom.dto.category.CategoryDTO;
+import com.nik.code.ecom.model.Category;
+import com.nik.code.ecom.utils.DTOMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryDTOMapper implements DTOMapper<CategoryDTO, Category> {
+
+
+    @Override
+    public Category toModel(CategoryDTO instance) {
+        return null;
+    }
+
+    @Override
+    public CategoryDTO toDTO(Category instance) {
+        Integer parentId = instance.getParent() != null? instance.getParent().getId() : null;
+        return new CategoryDTO(instance.getName(), instance.getDescription(), instance.getImageURL(), parentId);
+    }
+
+    @Override
+    public List<Category> toModel(List<CategoryDTO> instance) {
+        return null;
+    }
+
+    @Override
+    public List<CategoryDTO> toDTO(List<Category> instance) {
+        List<CategoryDTO> categoryDTOS = new ArrayList<>();
+        for (Category category: instance){
+            categoryDTOS.add(toDTO(category));
+        }
+        return categoryDTOS;
+    }
+}
