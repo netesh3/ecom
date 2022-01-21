561b48b9efcbfd177ea79febffa92da6cccfc12e
Kumar <netkumar@informatica.com>
2022-01-22 01:25:37 +0530

    search with keyword

--- src/main/java/com/nik/code/ecom/repository/SearchRepository.java
+++ src/main/java/com/nik/code/ecom/repository/SearchRepository.java
@@ -2,6 +2,7 @@ package com.nik.code.ecom.repository;
 
 
 import com.nik.code.ecom.model.Product;
+import com.sun.xml.bind.v2.TODO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,7 +13,15 @@ import java.util.List;
 @Repository
 public interface SearchRepository extends PagingAndSortingRepository<Product, Integer> {
 
+    /*
+        TODO
+        use Soundex and Levenshtein algorithm for searching
+     */
     @Query(value = "SELECT * FROM Product WHERE :keyword <% concat_ws(' ', name, description, brand) limit 5", nativeQuery = true)
     List<Product> searchProducts(String keyword);
-    //https://dev.to/moritzrieger/build-a-fuzzy-search-with-postgresql-2029
+
+    /*
+        Reference https://dev.to/moritzrieger/build-a-fuzzy-search-with-postgresql-2029
+     */
+
 }
